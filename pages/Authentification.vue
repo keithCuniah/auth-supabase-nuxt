3f16c6483cd974f2c6214b4cac7cebac96cca5ca
keithCuniah <keithcuniah@gmail.comh>
2022-10-05 10:38:32 +0200

    managing error : email not confirmed and invalid login credential

--- pages/Authentification.vue
+++ pages/Authentification.vue
@@ -50,15 +50,24 @@ export default {
       const { user, error } = await this.$supabase.auth.signUp({
         ...this.credential,
       });
-      console.log({ ...this.credential });
       this.$store.commit('ON_AUTH_STATE_CHANGED_MUTATION', user);
       console.log('signUp', user, error);
     },
     async login() {
-      const { user, error } = await this.$supabase.auth.signIn({
-        ...this.credential,
-      });
-      console.log('signIn', user, error);
+      try {
+        const { user, error } = await this.$supabase.auth.signIn({
+          ...this.credential,
+        });
+        if (error) {
+          alert(`Error ${error.status}: ${error.message}`);
+        }
+        if (user) {
+          this.$store.commit('ON_AUTH_STATE_CHANGED_MUTATION', user);
+        }
+        console.log('signIn', user, error);
+      } catch (err) {
+        alert(err);
+      }
     },
   },
 };
