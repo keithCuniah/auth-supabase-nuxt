d5aaee100ecdecd0160c85cdfc92eeacd1f91637
keithCuniah <keithcuniah@gmail.comh>
2022-10-06 18:15:05 +0200

    clean code

--- store/index.js
+++ store/index.js
@@ -4,11 +4,7 @@ export const state = () => ({
 
 export const mutations = {
   ON_AUTH_STATE_CHANGED_MUTATION: (state, user) => {
-    if (user) {
-      state.user = user;
-    } else {
-      state.user = false;
-    }
+    state.user = user || false;
   },
 };
 
